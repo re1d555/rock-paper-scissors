0b319011862ea591d637234975a9f8d285f1e258
re1d555 <55630923+re1d555@users.noreply.github.com>
2024-01-03 21:16:57 +0300

    Add some more UI

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let score = 0;
+let computerScore = 0;
 const gameRes = document.querySelector('.gameResult');
 const res = document.querySelector('.result');
 const buttons = document.querySelectorAll('.btn');
 const buttonsDiv = document.querySelector('.playBtn');
+const resDiv = document.querySelector('.gameRes');
+const playerCount = document.querySelector('.player');
+const computerCount = document.querySelector('.computer');
 
 function getComputerChoice() {
 let randomChoice = (Math.floor(Math.random() * 3));
@@ -22,16 +26,36 @@ function playRound(playerSelection, computerSelection) {
   } else if (computerSelection === playerSelection) {
     return result = 'It`s a Tie!';
   } else {
-    return score += -1, result = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    return computerScore += 1, result = `You Lose! ${computerSelection} beats ${playerSelection}`;
   }
 }
 
 function gameScore() {
   if (score === 5) {
-    gameRes.innerText = `Game score: ${score}, You WIN!`;
-  } else if (score === -5) {
-    gameRes.innerText = `Game score: ${score}, You LOSE!`;
+    gameRes.innerText = `Player score: ${score}, You WIN!`;
+  } else if (computerScore === 5) {
+    gameRes.innerText = `Computer score: ${computerScore}, You LOSE!`;
   } 
+}
+
+function gameReset() {
+  if (gameRes.innerText != '') {
+    const reset = document.createElement('button');
+    reset.addEventListener('click', () => {
+      score = 0;
+      computerScore = 0;
+      document.querySelectorAll('.btn').forEach((button) => button.disabled = false);
+      gameRes.innerText = '';
+      playerCount.innerText = 'Player: ';
+      computerCount.innerText = 'Computer: ';
+      resDiv.removeChild(reset);
+    })
+    reset.textContent = 'Reset The Tournament';
+    reset.setAttribute('style', 'width: 130px; height: 60px; border-radius: 10px; font-weight: bold');
+    resDiv.appendChild(reset);
+    document.querySelectorAll('.btn').forEach((button) => button.disabled = true);
+    res.innerText = '';
+   }
 }
 
 buttons.forEach((button) => {
@@ -40,21 +64,8 @@ buttons.forEach((button) => {
    const computerChoice = getComputerChoice();
    res.innerText = playRound(playerChoice, computerChoice)
    gameScore()
-   if (gameRes.innerText != '') {
-    const reset = document.createElement('button');
-    reset.addEventListener('click', () => {
-      score = 0;
-      document.querySelectorAll('.btn').forEach((button) => button.disabled = false);
-      gameRes.innerText = '';
-      buttonsDiv.removeChild(reset);
-    })
-    reset.textContent = 'Reset The Tournament';
-    reset.style.width = '100px';
-    buttonsDiv.appendChild(reset);
-    document.querySelectorAll('.btn').forEach((button) => button.disabled = true);
-    res.innerText = '';
-   }
+   playerCount.innerText = `Player: ${score}`;
+   computerCount.innerText = `Computer: ${computerScore}`;
+   gameReset()
   });
 });
-
-
